2b7166e3da3747f0acac2974bd2c2b2745ee6f82
ririnto <ririnto@kakao.com>
2025-11-30 18:25:50 +0900

    refactor: improve init script and inline configurations
    - Update version selection logic for Rust, Golang, Node.js, and Python to dynamically select the latest versions using `grep -E` and `sort -V`.
    - Inline configuration files (`.default-cargo-crates`, `.profile`, `.zimrc`) directly into `init.zsh` to reduce file dependencies.
    - Fetch the latest `.zimrc` template from the official repository before appending custom modules.
    - Fix heredoc escaping for `.profile` generation.
    - Use POSIX-compliant `[ ... ]` for architecture checks.
    - Adjust `OLLAMA_NUM_PARALLEL` setting.

--- init.zsh
+++ init.zsh
@@ -7,7 +7,20 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 if [ -f $HOME/.default-cargo-crates ]; then
   mv $HOME/.default-cargo-crates $HOME/.default-cargo-crates.bak.$(date +%Y%m%d%H%M%S)
 fi
-cp $(dirname "$0")/.default-cargo-crates $HOME/.default-cargo-crates
+cat << "EOF" > $HOME/.default-cargo-crates
+bat
+bandwhich
+difftastic
+exa
+du-dust
+fd-find
+hexyl
+hwatch
+ripgrep
+sd
+svgbob_cli
+xh
+EOF
 
 if [ -f $HOME/.p10k.zsh ]; then
   mv $HOME/.p10k.zsh $HOME/.p10k.zsh.bak.$(date +%Y%m%d%H%M%S)
@@ -17,12 +30,42 @@ cp $(dirname "$0")/.p10k.zsh $HOME/.p10k.zsh
 if [ -f $HOME/.profile ]; then
   mv $HOME/.profile $HOME/.profile.bak.$(date +%Y%m%d%H%M%S)
 fi
-cp $(dirname "$0")/.profile $HOME/.profile
+cat << "EOF" > $HOME/.profile
+# -*- coding: utf-8 -*-
+
+export OLLAMA_MAX_LOADED_MODELS=2
+export OLLAMA_CONTEXT_LENGTH=8192
+export OLLAMA_NUM_PARALLEL=2
+export ANTHROPIC_BASE_URL="http://127.0.0.1:3456"
+export ANTHROPIC_AUTH_TOKEN="ignore-me"
+
+if [ $(uname -m) = "x86_64" ]; then
+  HOMEBREW_PREFIX="/usr/local"
+else
+  HOMEBREW_PREFIX="/opt/homebrew"
+fi
+
+if [ -f "$HOMEBREW_PREFIX/bin/brew" ]; then
+  eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
+fi
+EOF
 
 if [ -f $HOME/.zimrc ]; then
   mv $HOME/.zimrc $HOME/.zimrc.bak.$(date +%Y%m%d%H%M%S)
 fi
-cp $(dirname "$0")/.zimrc $HOME/.zimrc
+curl -o $HOME/.zimrc https://raw.githubusercontent.com/zimfw/zimfw/refs/heads/master/src/templates/zimrc
+
+cat << "EOF" >> $HOME/.zimrc
+zmodule asdf
+zmodule fasd
+zmodule fzf
+zmodule gradle/gradle-completion
+zmodule joke/zim-minikube
+zmodule joke/zim-yq
+zmodule homebrew --if-ostype 'darwin*'
+zmodule ohmyzsh/ohmyzsh --root plugins/vim-interaction
+zmodule romkatv/powerlevel10k --use degit
+EOF
 
 if [ ! -f $HOME/.ssh/id_ed25519 ]; then
   ssh-keygen -t ed25519 -f $HOME/.ssh/id_ed25519 -N ""
@@ -75,7 +118,7 @@ fi
 sudo xcode-select --install
 sudo softwareupdate --install-rosetta --agree-to-license
 
-if [[ $(uname -m) == "x86_64" ]]; then
+if [ $(uname -m) = "x86_64" ]; then
   HOMEBREW_PREFIX="/usr/local"
 else
   HOMEBREW_PREFIX="/opt/homebrew"
@@ -104,20 +147,20 @@ sed -i.bak "
 " "$HOME/.zshrc"
 
 asdf plugin add rust
-asdf install rust stable
-asdf set -u rust stable
+asdf install rust $(asdf list all rust | grep -E '^[0-9]+(\.[0-9]+){1,2}$' | sort -V | tail -n 1)
+asdf set -u rust $(asdf list all rust | grep -E '^[0-9]+(\.[0-9]+){1,2}$' | sort -V | tail -n 1)
 
 asdf plugin add nodejs
-asdf install nodejs latest:24
-asdf set -u nodejs latest:24
+asdf install nodejs $(asdf list all nodejs | grep -E '^24(\.[0-9]+){1,2}$' | sort -V | tail -n 1)
+asdf set -u nodejs $(asdf list all nodejs | grep -E '^24(\.[0-9]+){1,2}$' | sort -V | tail -n 1)
 
 asdf plugin add golang
-asdf install golang latest
-asdf set -u golang latest
+asdf install golang $(asdf list all golang | grep -E '^[0-9]+(\.[0-9]+){1,2}$' | sort -V | tail -n 1)
+asdf set -u golang $(asdf list all golang | grep -E '^[0-9]+(\.[0-9]+){1,2}$' | sort -V | tail -n 1)
 
-asdf list all python
-asdf install python latest:3.13
-asdf set -u python latest:3.13
+asdf plugin add python
+asdf install python $(asdf list all python | grep -E '^3\.13(\.[0-9]+){0,1}$' | sort -V | tail -n 1)
+asdf set -u python $(asdf list all python | grep -E '^3\.13(\.[0-9]+){0,1}$' | sort -V | tail -n 1)
 
 # mkdir -p $HOME/Projects/local
 # mkdir -p $HOME/.minikube/certs
